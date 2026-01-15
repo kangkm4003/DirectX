f5a59d4cd25b98342cac5cb560f6ff75c36641e0
kangk <kangk@NOOB1_DEVICE>
2026-01-15 18:51:36 +0900

    Todo: indices 완성하기

--- D2D/Scenes/Scene.h
+++ D2D/Scenes/Scene.h
@@ -8,6 +8,30 @@ using namespace std;
 class Scene
 {
 public:
+	virtual ~Scene() = default;
+
+	virtual void InitScene() = 0;
+	virtual void Destroy() = 0;
+
+	virtual void Update()
+	{
+		for (const auto& obj : objects)
+		{
+			obj->Update();
+		}
+	}
+	virtual void Render()
+	{
+		for (const auto& obj : objects)
+		{
+			obj->Render();
+		}
+	}
+
+	void AddObject(std::shared_ptr<Object> object)
+	{
+		objects.push_back(object);
+	}
 
 private:
 	vector<shared_ptr<Object>> objects;
--- D2D/Utilities/GeometryHelper.cpp
+++ D2D/Utilities/GeometryHelper.cpp
@@ -32,21 +32,23 @@ namespace GeometryHelper
 		{
 
 			FLOAT theta = 2 * XM_PI / quality;
-			vector<Vertex> vertices(quality);
-
+			vector<Vertex> vertices(quality + 1); //인덱스 0번은 원의 중점
+			vector<UINT> indices(quality * 3); // 삼각형을 quality만큼 그린다
 			vertices[0].position = Vector2(0.f, 0.f);
-			for (UINT i = 1; i < quality; i++)
+			for (UINT i = 1; i < quality + 1; i++)
 			{
-				vertices[i].position = Vector2((sinf(theta / i), cosf(theta / i)));
+				vertices[i].position = Vector2((sinf(theta * (i - 1)), cosf(theta * (i - 1)))) * 0.5; //로컬좌표를 -1 ~ 1 까지로 축소
 
+				indices.push_back(0);
+				indices.push_back(i);
+				indices.push_back(i+1);
 			}
-			vector<UINT> indices = { 0, 1, 2, 0, 2, 3, 0, 3, 4 };
 
 			mesh = make_shared<Mesh>();
 			mesh->Create(vertices, indices);
 
 		}
-
+		
 		return mesh;
 	}
 	shared_ptr<Mesh> CreateSolidColorCircle(UINT quality)
