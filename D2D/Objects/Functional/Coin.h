5f2c1795a7fd13350f958d01c8f39280f9adb3b3
kangk <kangk@NOOB1_DEVICE>
2026-04-27 18:02:44 +0900

    코인 원을 렌더링 하도록 매시와 렌더러 컴포넌트 추가 및 precache

--- D2D/Objects/Functional/Coin.h
+++ D2D/Objects/Functional/Coin.h
@@ -16,6 +16,13 @@ public:
 	Vector2 reSpawnPosMin = Vector2(WIN_DEFAULT_WIDTH + 300.f, 200.f); //코인이 다시 스폰될 때의 좌표 최소값 (화면 중앙 기준)
 	Vector2 reSpawnPosMax = Vector2(WIN_DEFAULT_WIDTH + 1000.f, 400.f); //코인이 다시 스폰될 때의 좌표 최대값 (화면 중앙 기준)
 	//
+
+	void onCollision(shared_ptr<Collider> target) override;
+
+	//Component Precache
 private:
-	void onCollision(shared_ptr<Collider> target) override; //충돌했을 때의 행동
+	shared_ptr<class Material> material = nullptr;
+	shared_ptr<class MeshRenderer> meshRenderer = nullptr;
+	shared_ptr<class ScrollObject> scrollObject = nullptr;
+	//shared_ptr<class Transform> transform = nullptr; //이미 CollisionObject에서 precache 되어 있음
 };
