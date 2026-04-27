c9587abb2dab94bae9bd4674a46067c334b7b2d9
kangk <kangk@NOOB1_DEVICE>
2026-04-27 18:04:05 +0900

    충돌계산을 시작하는 거리를 추가 함으로서 성능 최적화. (타겟의 Transform도 동시에 저장하므로 용량 다소 상)

--- D2D/Objects/Functional/CollisionObject.cpp
+++ D2D/Objects/Functional/CollisionObject.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 
-//Components
-#include "Components/Collision/Collider.h"
-//
+#include "Components/Transform.h"
 
 #include "CollisionObject.h"
 
@@ -11,7 +9,6 @@ CollisionObject::CollisionObject(Vector2 position, Vector2 scale, float rotation
 {
 
 	//Component Precache
-	collider = GetComponent<Collider>("Collider");
 	transform = GetTransform();
 	//
 }
@@ -21,13 +18,19 @@ void CollisionObject::Update()
 	SUPER::Update();
 
 	if (collider == nullptr) return; //Collider 컴포넌트가 없다면 충돌 검사 불가능
+	if (transform == nullptr) return; //Transform 컴포넌트가 없다면 충돌 검사 불가능
 	if (targets.empty()) return; //충돌 대상이 없다면 충돌 검사 불필요
 
 	for (const auto& target : targets)
 	{
-		if (target.second == nullptr) continue; //충돌 대상이 유효하지 않다면 검사 불필요
-
 		auto targetCollider = target.second;
+		auto targetTransform = targetTransforms[target.first];
+
+		if (targetCollider == nullptr) continue; //Collider 컴포넌트가 유효하지 않다면 검사 불필요
+		if (targetTransform == nullptr) continue; //충돌 대상의 Transform 컴포넌트가 유효하지 않다면 거리 계산 불가능
+
+		if ((targetTransform->GetPosition() - transform->GetPosition()).LengthSquared() > startCalculationDistance * startCalculationDistance)
+			continue; //충돌 계산 시작 거리보다 멀리 있는 대상이라면 충돌 계산 불필요
 		if (targetCollider == nullptr) continue; //충돌 대상에 Collider 컴포넌트가 없다면 충돌 검사 불가능
 
 		if (collider->IsColliding(targetCollider))
@@ -39,7 +42,7 @@ void CollisionObject::Update()
 
 void CollisionObject::AddTarget(const string& name, shared_ptr<Collider> target) //부딛힐 대상 추가
 {
-	for (auto member : targets) //같은 이름을 가진 겍체가 이미 존재하는지 검사
+	for (auto member : targets) //같은 이름을 가진 객체가 이미 존재하는지 검사
 	{
 		if (member.first == name)
 		{
@@ -48,15 +51,17 @@ void CollisionObject::AddTarget(const string& name, shared_ptr<Collider> target)
 		}
 	}
 	targets.insert({ name, target });
+	targetTransforms.insert({ name, target->GetOwner()->GetTransform() });
 }
 
 void CollisionObject::RemoveTarget(const string& name)
 {
-	for (auto member : targets) //같은 이름을 가진 겍체가 이미 존재하는지 검사
+	for (auto member : targets) //같은 이름을 가진 객체가 이미 존재하는지 검사
 	{
 		if (member.first == name)
 		{
 			targets.erase(name);
+			targetTransforms.erase(name);
 			return;
 		}
 	}
