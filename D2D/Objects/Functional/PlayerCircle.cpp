2a841f1366404679263a54278c604d3cddd57665
kangk <kangk@NOOB1_DEVICE>
2026-05-09 18:03:10 +0900

    Fever 컴포넌트 적용. 조작키 설정. 히트 혹은 피버 상태일시 Health 컴포넌트를 활용해 무적시간 추가. 히트시 잠시 색상 변경.

--- D2D/Objects/Functional/PlayerCircle.cpp
+++ D2D/Objects/Functional/PlayerCircle.cpp
@@ -8,6 +8,7 @@
 #include "Components/Transform.h"
 #include "Components/Input/Jump.h"
 #include "Components/System/Health.h"
+#include "Components/Input/Fever.h"
 
 #include "Utilities/Random.h"
 #include "Utilities/GeometryHelper.h"
@@ -24,46 +25,67 @@ PlayerCircle::PlayerCircle(Vector2 position, Vector2 scale, Color color, UINT se
 	boxCollider = make_shared<BoxCollider>();
 	jump = make_shared<Jump>();
 	health = make_shared<Health>();
+	fever = make_shared<Fever>();
 
 	//Component Setup
 	meshRenderer->SetMesh(GeometryHelper::CreateColorCircle(segments)); //원으로 만들기 위해 MeshRenderer에 원 메쉬 설정
 	meshRenderer->SetShaderSet(SHADERS->GetShader(L"./_Shaders/Vertex.hlsl", Vertex::descs));
-	jump->maxJumpCount = 2;
+	jump->SetMappingKey(VK_SPACE);//점프키를 스페이스바로 지정
+	jump->maxJumpCount = 2; //최대점프횟수를 2회로 설정 (이단점프)
+	fever->SetMappingKey(VK_LSHIFT); //피버 시작키를 왼쪽 쉬프트로 지정
+	fever->SetFeveringTime(10.f);
+	
+
+	originalColor = color;
 
 	//Component Registration
 	AddComponent(circleCollider);
 	AddComponent(boxCollider);
 	AddComponent(health);
 	AddComponent(jump);
+	AddComponent(fever);
 }
 
 void PlayerCircle::Update()
 {
 	SUPER::Update();
+
+	if (!health->GetisImmute() && immuteEnd_Dirty) ImmuteEnd(); //무적시간 종료
+
+	if (INPUT->Down(feverKey) && fever->GetinFever())
+	{
+		health->SetImmute(10.f);
+	}
 }
 
 void PlayerCircle::Render()
 {
 	SUPER::Render();
-
-	if (inFever)
-	{
-		if (feverColorTimer >= feverColorTime)
-		{
-			GetComponent<Material>("Material")->SetColor(Random::GetColor());
-			feverColorTimer = 0;
-		}
-		else
-			feverColorTimer += DELTA;
-	}
 }
 
-void PlayerCircle::onCollision(shared_ptr<Collider> target)
+void PlayerCircle::onCollision(shared_ptr<Collider> target) //장해물과 부딪힘. 무적상태가 아니라면 플레이어에게 데미지
 {
-	GetComponent<Material>("Material")->SetColor(damegeColor);
+	if (!health->GetisImmute())
+	{
+		material->SetColor(damegeColor); //플레이어의 색상을 데미지를 입었을시에 색상으로 변경
+		health->Damage(1); //데미지
+		immuteEnd_Dirty = true; //무적시간 종료를 Update함수에서 확인 하기 때문에 1회만 실행하기 위해서 Dirty값을 활용 
+	}
 }
 
 void PlayerCircle::DoJump(float amount)
 {
 	jump->DoJump(amount);
+}
+
+void PlayerCircle::StartFever()
+{
+	meshRenderer->SetMesh(GeometryHelper::CreateRectangle());
+	transform->SetScale(transform->GetScale() * 2); //피버 시작시 원본 크기의 2배로 설정
+}
+
+void PlayerCircle::ImmuteEnd()
+{
+	immuteEnd_Dirty = false; //Dirty값을 false로 변경해 함수가 매 Update마다 실행되는것을 방지
+	material->SetColor(originalColor); //원래 색상으로 돌아감
 }
--- D2D/Objects/Functional/PlayerCircle.h
+++ D2D/Objects/Functional/PlayerCircle.h
@@ -1,11 +1,10 @@
 #pragma
 #include "Objects/Functional/CollisionObject.h"
 
-//ToDo : Jump와 Controlloer 컴포넌트 구현 (컴포넌트에서 키를 누르면 owner에게 신호 전달하는 형태)
 class PlayerCircle : public CollisionObject
 {
 public:
-	PlayerCircle(Vector2 position, Vector2 scale, Color color = RED, UINT segments = 50);
+	PlayerCircle(Vector2 position, Vector2 scale, Color color = GREEN, UINT segments = 50);
 	~PlayerCircle() override = default;
 
 	void Update() override;
@@ -14,11 +13,20 @@ public:
 	void onCollision(shared_ptr<Collider> target) override; //충돌했을 때의 행동
 
 	void DoJump(float amount);
+	void StartFever();
+
+	void ImmuteEnd();
 
 private:
 
 	bool immuteEnd_Dirty = false; //플레이어의 무적 상태 종료 이벤트를 종료 직후 1회만 실행하기 위한 변수값
 	Color damegeColor = RED; //데미지를 받았을시 바뀔 색상
+	Color originalColor = GREEN; //색상이 바뀌고 돌아갈때의 색상
+
+	//조작키
+	int jumpKey = VK_SPACE;
+	int feverKey = VK_LSHIFT;
+
 	//Component Precache
 private:
 	shared_ptr<class Material> material;
@@ -27,5 +35,6 @@ private:
 	shared_ptr<class BoxCollider> boxCollider;
 	shared_ptr<class Jump> jump;
 	shared_ptr<class Health> health;
+	shared_ptr<class Fever> fever;
 
 };
